6f11add0ae32931030ae9d4b5764107338e34d28
xMathyu <xmathyu@gmail.com>
2025-04-10 00:56:59 -0500

    feat: our services section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,15 +3,17 @@ import ContactUs from "@/pageComponents/contactUs/contactUs";
 import { Footer } from "@/pageComponents/footer/footer";
 import HeroSection from "@/pageComponents/hero/Hero";
 import Projects from "@/pageComponents/projects/projects";
+import { ServicesSection } from "@/components/ServicesSection";
 
 export default function Home() {
   return (
-    <>
+    <main className="overflow-hidden">
       <HeroSection />
       <AboutUs />
+      <ServicesSection />
       <Projects />
       <ContactUs />
       <Footer />
-    </>
+    </main>
   );
 }
